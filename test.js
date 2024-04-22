3877dd6010d26cecd12cfbab107fa8feae9f4268
xuanquan <xuanquan@kingsoft.com>
2024-04-22 14:36:09 +0800

    提交

---
+++ test.js
@@ -0,0 +1,25 @@
+function dobounce(fn,ms){
+    let timer = null
+    return function(...args){
+        const context = this
+        if(timer){
+            clearTimeout(timer)
+        }
+        timer = setTimeout(()=>{
+            fn.apply(context,...args)
+        },ms)
+    }
+}
+
+function throttle(fn,ms){
+    let timer = null
+    return function(...args){
+        const context = this
+        if(!timer){
+            timer = setTimeout(()=>{
+                timer = null
+                fn.apply(context,...args)
+            },ms)
+        }
+    }
+}
